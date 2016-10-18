aa957c0cef38750a7acb96854ad061c5dff181aa
mjobin2 <mjobin2@nait.ca>
2016-10-18 15:27:41 -0600

    Fresh copy

---
+++ ChinookSample/ChinookSystem/Security/UserManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework;    //UserStore
+using Microsoft.AspNet.Identity;                    //UserManager
+#endregion
+namespace ChinookSystem.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+
+        //setting up the default webMaster
+        #region Constants
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        private const string STR_EMAIL_FORMAT = "{0}@Chinook.ca";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+        public void AddWebmaster()
+        { 
+            if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                var webMasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+                //this Create command is from the inherited UserManager class
+                //this command creates a record on the security Users table (AspNetUsers)
+                this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+                //this AddToRole command is from the inherited UserManager class
+                //this command creates a record on the security UserRole table (AspNetUserRoles)
+                this.AddToRole(webMasterAccount.Id, SecurityRoles.WebsiteAdmins);
+            }
+        }
+    }//eoc
+}//eon
