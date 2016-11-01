2e70be7b46d3f1f216496eb1b4de5fe472838d2f
mjobin2 <mjobin2@nait.ca>
2016-11-01 16:45:35 -0600

    CRUD practice, validation and exception handling

--- ChinookSample/ChinookSystem.Data/Entities/Track.cs
+++ ChinookSample/ChinookSystem.Data/Entities/Track.cs
@@ -25,15 +25,34 @@ namespace ChinookSystem.Data.Entities
         //property names should use sql attribute name
         //properties should be listed in the same order
         //     as sql table attributes for easy of maintenance
+
+        //entity validation
+        //this is validation that kicks in when the .SaveChange() command is executed
+        //[Required(ErrorMessage="message")]
+        //[StringLength(int maximum[,int minimum], ErrorMessage="message")]
+        //[Range(double minimum, double maximum, ErrorMessage="message")]
+        //[RegularExpression("expression", Errormessage="message")]
         [Key]
         public int TrackId { get; set; }
+        [Required(ErrorMessage ="Name is a required field")]
+        [StringLength(200, ErrorMessage ="Name is too long. Max characters: 200")]
         public string Name { get; set; }
+        [Range(1.0, double.MaxValue, ErrorMessage ="Invalid Album. Try selection again.")]
         public int? AlbumId { get; set; }
+        [Required(ErrorMessage = "Media Type is a required field")]
+        [Range(1.0, double.MaxValue, ErrorMessage = "Invalid Media Type. Try selection again.")]
         public int MediaTypeId { get; set; }
+        [Range(1.0, double.MaxValue, ErrorMessage = "Invalid Genre. Try selection again.")]
         public int? GenreId { get; set; }
+        [StringLength(220, ErrorMessage = "Composer is too long. Max characters: 220")]
         public string Composer { get; set; }
+        [Required(ErrorMessage = "MSec is a required field")]
+        [Range(1.0, double.MaxValue, ErrorMessage = "Invalid MSec. Must be greater than 0.")]
         public int Milliseconds { get; set; }
+        [Range(1.0, double.MaxValue, ErrorMessage = "Invalid Bytes. Must be greater than 0.")]
         public int? Bytes { get; set; }
+        [Required(ErrorMessage = "Price is a required field")]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Invalid Price. Price must be 0 or greater.")]
         public decimal UnitPrice { get; set; }
 
         //navigation properties for use by Linq
